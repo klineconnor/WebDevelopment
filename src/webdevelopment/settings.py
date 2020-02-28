e615d613904b3f3bc3ec8f54c8a06f06b2ca674a
klineconnor <klineconnor@yahoo.com>
2020-02-28 10:27:35 -0600

    add home page

--- src/webdevelopment/settings.py
+++ src/webdevelopment/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # third party
 
     # own
+    'pages',
     'products',
 ]
 
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'webdevelopment.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
